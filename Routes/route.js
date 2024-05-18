f68568cbf9f7d75cb7e85a5c9a3268d029bc123d
Arnavgarg68 <arnavgarg221@gmail.com>
2024-05-18 11:48:24 +0530

    fix v2

--- Routes/route.js
+++ Routes/route.js
@@ -154,7 +154,7 @@ router.delete("/delete/:id/:taskId",jwtVerify,async(req,res)=>{
 // create team room
 router.get('/create-team/:groupId',async(req,res)=>{
     const {groupId} = req.params;
-    if(typeof(groupId!=Number)){
+    if(!/^\d+$/.test(groupId)){
         res.status(200).json({error:"Enter numeric value only"})
         return;
     }
